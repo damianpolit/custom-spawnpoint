4ff9d47ca0656df32cb689e6d1c1ebc7297847d0
Damian <damianpolit@interia.pl>
2023-03-06 21:03:19 +0100

    added functionality for firstjoin spawnpoint event

--- src/main/java/com/dpolit/customspawnpoint/listeners/PlayerJoinListener.java
+++ src/main/java/com/dpolit/customspawnpoint/listeners/PlayerJoinListener.java
@@ -1,13 +1,20 @@
 package com.dpolit.customspawnpoint.listeners;
 
+import com.dpolit.customspawnpoint.domain.SpawnEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import static com.dpolit.customspawnpoint.util.PluginUtil.getTeleportationCoords;
 
 public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(final PlayerJoinEvent event) {
-        //
+        Player player = event.getPlayer();
+        if (!player.hasPlayedBefore()) {
+            player.teleport(getTeleportationCoords(SpawnEvent.FIRSTJOIN));
+        }
     }
 }
